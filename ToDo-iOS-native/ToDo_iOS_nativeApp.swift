015a09000082eb75d8dbdd18f707540c0e35b326
Homit Dalia <homitdalia59@gmail.com>
2024-03-07 00:18:30 +0530

    Initial Commit

---
+++ ToDo-iOS-native/ToDo_iOS_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_iOS_nativeApp.swift
+//  ToDo-iOS-native
+//
+//  Created by Homit Dalia on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_iOS_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
